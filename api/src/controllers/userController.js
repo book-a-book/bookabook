222e363349bc6a41ae8c4bb3bcba356b4f4020c3
joartola <31057068+joartola@users.noreply.github.com>
2018-11-06 19:53:22 -0300

    User login register (#7)
    * Login & Register working
    
    * Added ensureAuthenticated in tokenHandler
    
    * Fixed Register Issue (Required Fields)

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -8,16 +8,28 @@ const User = mongoose.model('User', UserSchema);
 export const userRegist = (req, res) => {
 
     var user = new User({
+        name: req.body.name,
+        lastName: req.body.lastName,
         username: req.body.username,
         password: ''
       });
   user.password = user.generateHash(req.body.password);
   user.save(function(err) {
+    if(err)
+    {
+      console.log(err)
+      return res.status(400).send({ message: err});
+    }
+
     return res.status(200).send({ message: "ok"});
   });
 };
 
 export const userLogin = (req, res) => {
+  console.log(req.body);
+    User.findOne({ username: req.body.username }, function(err, user) {
+      console.log(err);
+      console.log(user);
     User.findOne({ username: req.body.username }, function(err, user) {
       if (!user.validPassword(req.body.password)) {
         res.status(400).json({ message: "Bad credentials" });
