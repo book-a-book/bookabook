14472a7183c3a1b5fbc8de161a75023c223c0803
JARTOIA-UY\jartoia <artolajoaquin93@gmail.com>
2018-11-10 17:14:32 -0300

    changed to CRM distribution by modules, auth at level of end point, added moment dependency

--- api/index.js
+++ api/index.js
@@ -1,34 +1,34 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import userRoutes from './src/routes/userRoutes';
-import routes from './src/routes/bookRoutes';
-
-var middleware = require("./src/services/tokenHandler");
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+const routes = require("./src/routes");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 
 // mongoose connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/CRMdb', {
+mongoose.connect(
+  "mongodb://localhost/CRMdb",
+  {
     useMongoClient: true
-});
+  }
+);
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(middleware.ensureAuthenticated);
+app.use(cors);
 routes(app);
-userRoutes(app);
 
 // serving static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) =>
-    res.send(`Node and express server is running on port ${PORT}`)
+app.get("/", (req, res) =>
+  res.send(`Node and express server is running on port ${PORT}`)
 );
 
 app.listen(PORT, () =>
-    console.log(`Back-end server is running on port ${PORT}`)
+  console.log(`Back-end server is running on port ${PORT}`)
 );
---
+++ api/src/middleware/authenticate.js
@@ -0,0 +1,29 @@
+import { UserSchema } from "../crm/user/userModel";
+import mongoose from "mongoose";
+
+var jwt = require("jwt-simple");
+var moment = require("moment");
+var config = require("../config/config");
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = function(req, res, next) {
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "You need to include token" });
+  }
+
+  var token = req.headers.authorization.split(" ")[1];
+  var payload = jwt.decode(token, config.TOKEN_SECRET);
+
+  if (payload.exp <= moment().unix()) {
+    return res.status(401).send({ message: "The token is expired" });
+  }
+
+  var user = User.findById(payload.sub);
+  if (!user) {
+    return res.status(401).send({ message: "Invlid Request" });
+  }
+
+  req.user = payload.sub;
+  next();
+};
