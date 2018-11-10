38381fc28a6a3b474ba71c79025d50ec127d99c1
Max Alegre <max8abreu13@gmail.com>
2018-11-10 18:44:52 -0300

    Merge conflicts

--- web/src/app/components/home/home.component.ts
+++ web/src/app/components/home/home.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public imagesUrl;
+
   constructor() { }
 
   ngOnInit() {
+    this.imagesUrl = [
+      'assets/images/Image_1.png',
+      'assets/images/Image_2.png',
+      'assets/images/Image_3.png',
+      'assets/images/Image_4.png',
+      'assets/images/Image_5.png',
+      // 'assets/images/Image_6.png',
+    ];
   }
 
 }
