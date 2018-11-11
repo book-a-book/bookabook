43ba0d636039c5d4be6cb8475912892dfb1296d6
Max Alegre <max8abreu13@gmail.com>
2018-11-11 18:01:31 -0300

    Merge conflicts

--- web/src/app/components/home/home.component.ts
+++ web/src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from 'src/app/services/book.service';
+import { Book } from 'src/app/models';
+import { AppConfig } from 'src/app/app.config';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public books: Book[];
   public imagesUrl;
 
-  constructor() { }
+  constructor(private bookService: BookService, private config: AppConfig) { }
 
   ngOnInit() {
     this.imagesUrl = [
@@ -20,6 +24,18 @@ export class HomeComponent implements OnInit {
       'assets/images/Image_5.png',
       // 'assets/images/Image_6.png',
     ];
+
+    this.bookService.getAll()
+      .subscribe(books => {
+        this.books = books;
+        this.processBooks();
+      });
+  }
+
+  processBooks() {
+    this.books.forEach(book => {
+      book.picture = book.picture.replace('public', `${this.config.apiUrl}`);
+    });
   }
 
 }
