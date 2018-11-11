b2cc432c339d32e161b2160a8bf32c615d173a2a
felipeolifre <foliverafreire@gmail.com>
2018-11-11 23:08:53 +0000

    Debug SSH key issues

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -5,4 +5,7 @@ set -x # Show the output of the following commands (useful for debugging)
 openssl aes-256-cbc -K $encrypted_a6f7567b205a_key -iv $encrypted_a6f7567b205a_iv -in deploy_rsa.enc -out deploy_rsa -d
 rm deploy_rsa.enc # Don't need it anymore
 chmod 600 deploy_rsa
+ls
 mv deploy_rsa ~/.ssh/id_rsa
+ls -a ~/
+ls ~/.ssh/
