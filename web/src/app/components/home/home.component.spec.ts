299a75d6a589e6c07d7e50b62920dba18d8cd0e9
Max Alegre <max8abreu13@gmail.com>
2018-11-11 19:47:38 -0300

    Test fixes

--- web/src/app/components/home/home.component.spec.ts
+++ web/src/app/components/home/home.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { BookService } from 'src/app/services/book.service';
+import { HttpClientModule } from '@angular/common/http';
+import { AppConfig } from 'src/app/app.config';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,9 +11,11 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [HomeComponent],
+      imports: [HttpClientModule],
+      providers: [BookService, AppConfig]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
