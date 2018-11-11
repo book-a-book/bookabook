3eb21e41ed3dbc75f7f7de8b753a1b40b1b3d18b
Max Alegre <max8abreu13@gmail.com>
2018-11-11 20:46:04 -0300

    Test fixes

--- web/src/app/components/login/login.component.spec.ts
+++ web/src/app/components/login/login.component.spec.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login.component';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -12,8 +13,8 @@ describe('LoginComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      imports: [FormsModule],
-      providers: [AuthenticationService, HttpClientModule]
+      imports: [FormsModule, HttpClientModule, RouterTestingModule],
+      providers: [AuthenticationService]
     })
       .compileComponents();
   }));
