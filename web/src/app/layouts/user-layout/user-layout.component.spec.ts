2e86633667aea3c0eda6a7761ffaeb26a004799f
Max Alegre <max8abreu13@gmail.com>
2018-11-11 19:54:39 -0300

    Test fixes

--- web/src/app/layouts/user-layout/user-layout.component.spec.ts
+++ web/src/app/layouts/user-layout/user-layout.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserLayoutComponent } from './user-layout.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 describe('UserLayoutComponent', () => {
   let component: UserLayoutComponent;
@@ -12,6 +13,7 @@ describe('UserLayoutComponent', () => {
     TestBed.configureTestingModule({
       declarations: [UserLayoutComponent],
       imports: [RouterTestingModule],
+      providers: [AuthenticationService],
       schemas: [NO_ERRORS_SCHEMA],
     })
       .compileComponents();
