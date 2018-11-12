13f5162fb262ad83fd1561f3e59bae7c3517af24
felipeolifre <foliverafreire@gmail.com>
2018-11-12 15:12:11 +0000

    Add automated deploy for the API

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -3,7 +3,6 @@ set -x
 if [ $TRAVIS_BRANCH == 'development' ] ; then
     # Initialize a new git repo in web/dist/BookABook, and push it to our server.
     cd web/dist/BookABook 
-    ls
     git init
         
     git remote add deploy "deploy@staging.bookabook.tk:/var/www/bookabook.tk/.git/"
@@ -13,6 +12,18 @@ if [ $TRAVIS_BRANCH == 'development' ] ; then
     git add .
     git commit -m "Deploy"
     git push --force deploy master
+
+    # Initialize a new git repo in api/, and push it to our server.
+    cd api/
+    git init
+
+    git remote add deploy "deploy@api.staging.bookabook.tk:/var/www/bookabook.tk/.git/"
+    git config user.name "Travis CI"
+    git config user.email "foliverafreire@gmail.com"
+
+    git add .
+    git commit -m "Deploy"
+    git push --force deploy master
 else
-    echo "Not deploying, since this branch isn't master."
+    echo "Not deploying, since this branch isn't development."
 fi
