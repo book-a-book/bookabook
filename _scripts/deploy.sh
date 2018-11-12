1923802272789a1a8cb74feee34e87ecec366b5a
felipeolifre <foliverafreire@gmail.com>
2018-11-12 15:37:33 +0000

    Fix script to deploy API

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -14,7 +14,7 @@ if [ $TRAVIS_BRANCH == 'development' ] ; then
     git push --force deploy-web master
 
     # Initialize a new git repo in api/, and push it to our server.
-    cd api/
+    cd ../../../api/
     git init
 
     git remote add deploy-api "deploy@api.staging.bookabook.tk:/var/www/bookabook.tk/.git/"
