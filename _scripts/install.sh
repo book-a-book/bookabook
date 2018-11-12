6f209efec2d3828ad41c7d4562ed01ea57ea132f
felipeolifre <foliverafreire@gmail.com>
2018-11-12 00:03:51 +0000

    Debug SSH key issues

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -4,7 +4,8 @@ set -x # Show the output of the following commands (useful for debugging)
 # Import the SSH deployment key
 pwd
 ls -l
-openssl aes-256-cbc -K $encrypted_a6f7567b205a_key -iv $encrypted_a6f7567b205a_iv -in deploy_rsa.enc -out deploy_rsa -d
+printenv
+openssl aes-256-cbc -K $encrypted_key -iv $encrypted_iv -in deploy_rsa.enc -out deploy_rsa -d
 rm deploy_rsa.enc # Don't need it anymore
 ls -l
 chmod 600 deploy_rsa
