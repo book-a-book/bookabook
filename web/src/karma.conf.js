f31f7fc51d5a89be883c6fdfb662d68b3c1b0d44
Max Alegre <max8abreu13@gmail.com>
2018-11-12 19:55:45 -0300

    Test fixes

--- web/src/karma.conf.js
+++ web/src/karma.conf.js
@@ -24,6 +24,7 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
+    browserDisconnectTimeout: 30000,
     autoWatch: true,
     singleRun: false,
     browsers: ['ChromeHeadless'],
