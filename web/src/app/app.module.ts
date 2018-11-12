53ed85116c04d162e052b7c08bf6c9c267ed20f9
gaston cerrillo <gastoncerrillo@gmail.com>
2018-11-11 21:10:13 -0300

    modal merge

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -13,7 +13,6 @@ import { routing } from './app.routing';
 import { AuthGuard } from './guards/auth.guard';
 import { EditBookModalComponent } from './components/edit-book-modal/edit-book-modal.component';
 import { NewBookModalComponent } from './components/new-book-modal/new-book-modal.component';
-import { FormsModule } from '@angular/forms';
 import { FeedbackRatingModalComponent } from './components/feedback-rating-modal/feedback-rating-modal.component'
 import { MDBBootstrapModule, ModalModule } from 'angular-bootstrap-md';
 import { BookService } from './services/book.service';
