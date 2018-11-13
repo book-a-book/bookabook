4196b1ec7920c262234e7f2a4c213d779f0ba96e
felipeolifre <foliverafreire@gmail.com>
2018-11-13 03:08:14 +0000

    Update SSH keys

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -2,7 +2,7 @@
 set -x # Show the output of the following commands (useful for debugging)
     
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_a6f7567b205a_key -iv $encrypted_a6f7567b205a_iv -in deploy_rsa.enc -out deploy_rsa -d
+openssl aes-256-cbc -K $encrypted_key -iv $encrypted_iv -in deploy_rsa.enc -out deploy_rsa -d
 rm deploy_rsa.enc # Don't need it anymore
 chmod 600 deploy_rsa
 mv deploy_rsa ~/.ssh/id_rsa
