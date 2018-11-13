b345193521d35d68630b6f9e0cbb59ff22feaa82
Max Alegre <max8abreu13@gmail.com>
2018-11-13 16:39:01 -0300

    Test fixes

--- web/src/app/components/home/home.component.spec.ts
+++ web/src/app/components/home/home.component.spec.ts
@@ -5,7 +5,7 @@ import { BookService } from 'src/app/services/book.service';
 import { HttpClientModule } from '@angular/common/http';
 import { AppConfig } from 'src/app/app.config';
 import { LoanModalComponent } from '../loan-modal/loan-modal.component';
-import { NO_ERRORS_SCHEMA } from '@angular/compiler/src/core';
+import { ModalModule } from 'angular-bootstrap-md';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -14,9 +14,8 @@ describe('HomeComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [HomeComponent, LoanModalComponent],
-      imports: [HttpClientModule],
-      providers: [BookService, AppConfig],
-      schemas: [NO_ERRORS_SCHEMA]
+      imports: [HttpClientModule, ModalModule.forRoot()],
+      providers: [BookService, AppConfig]
     })
       .compileComponents();
   }));
