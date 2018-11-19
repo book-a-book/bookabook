79bb97ff5e2bd29b4d5f6619b30e16023cb63d5d
Felipe Olivera <felipeolifre@users.noreply.github.com>
2018-11-19 19:28:06 -0300

    Deploy desde TravisCI a DigitalOcean (#37)
    * Change HTTP for HTTPS on API URL
    
    * Switch travis configuration to new stage server
    
    * Travis build for prod environment
    
    * Fix environments conf
    
    * Change HTTP for HTTPS on API URL
    
    * Switch travis configuration to new stage server
    
    * Travis build for prod environment
    
    * Fix environments conf
    
    * Change HTTP to HTTPS
    
    * Fix apiUrl for dev an prod environments
    
    * New deploy2prod script

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { isDevMode } from '@angular/core';
 
 export class AppConfig {
-    public readonly appName = 'BookABook';
-    public readonly navbarTitle = 'BookABook';
+    public readonly appName = 'Book A Book';
+    public readonly navbarTitle = 'Book A Book';
     public readonly navbarLogo = 'assets/icons/logo.png';
 
     get apiUrl() {
         const hostname = window.location.hostname;
-        const port = isDevMode() ? ':3000' : '/api';
-        return `http://${hostname}${port}`;
+	const apiUrl = isDevMode() ? 'http://${hostname}:3000' : 'https://${hostname}/api';
+        return apiUrl;
     }
 };
--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://api.bookabook.tk'
+  baseUrl: 'https://www.bookabook.tk'
 };
