d5a935cdb682c66ef32a517690fc1d44a5acaf33
gas4930 <gjcerrillo@netsuite.com>
2018-11-20 01:23:47 -0300

    working out

--- web/src/app/components/new-book-modal/new-book-modal.component.ts
+++ web/src/app/components/new-book-modal/new-book-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { Book } from 'src/app/models';
+import { BookService } from '../../services/book.service';
 @Component({
   selector: 'new-book-modal',
   templateUrl: './new-book-modal.component.html',
@@ -12,15 +13,23 @@ export class NewBookModalComponent implements OnInit {
   editorial: String;
   author: String;
   about: String;
-  launchDate: Number;
+  launchDate: Date;
   imgLink: String;
-  constructor() { }
+  constructor(
+    private bookService: BookService
+  ) { }
 
   ngOnInit() {
   }
 
   saveBook() {
-    console.log(this);
+    var book = new Book();
+    book.about = this.about;
+    book.author = this.author;
+    book.created_date = this.launchDate;
+    book.picture = this.imgLink;
+    book.title = this.title;
+    this.bookService.create(book);
     this.title = "";
     this.editorial = "";
     this.author = "";
